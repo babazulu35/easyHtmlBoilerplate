4381b88ba177a8e7c6c76d1d18546c2100c39ddf
babazulu35 <aysanhakan@gmail.com>
2015-08-26 00:00:50 +0300

    component added

---
+++ js/holdmyplace.js
@@ -0,0 +1,23 @@
+ /**
+* Easy Place Holder For all browser Tested on IE8 
+* by Hakan Hurriyet <aysanhakan@gmail.com>
+*/		
+
+    $('[placeholder]').focus(function() {
+  				var input = $(this);
+
+  				if(input.val() == input.attr('placeholder')) {
+  					input.val('');
+  					input.removeClass('placeholder');
+  				}
+  			}).blur(function() {
+  				var input = $(this);
+
+  				if(input.val() == '' || input.val() == input.attr('placeholder')) {
+
+  					input.addClass('placeholder');
+  					input.val(input.attr('placeholder'));
+  				}
+  			}).blur();
+
+  		});
